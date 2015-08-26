1f10dc33b4fe9f349ef7cf37846876442381a536
LIBERT Thomas <Thomas.LIBERT@ext.ec.europa.eu>
2015-08-26 16:50:03 +0200

    langague selector is back

--- themes/reps/templates/blocks/bean--reps_core_sb_right_image_link.tpl.php
+++ themes/reps/templates/blocks/bean--reps_core_sb_right_image_link.tpl.php
@@ -29,9 +29,9 @@
 ?>
 
 <div class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-  <?php if(isset($content['field_reps_core_image'])): ?>
+  <?php if(($content['field_reps_core_image'])): ?>
     <div class="content"<?php print $content_attributes; ?>>
-      <?php if (is_string($content['title_field'])): ?>
+      <?php if (is_string(render($content['title_field']))): ?>
   	    <h3><a href="<?php print (render($content['field_reps_core_external_url']));?>" target="_blank"><?php print (render($content['title_field']));?></a></h3>
       <?php endif;?>
 	  <a href="<?php print (render($content['field_reps_core_external_url']));?>" target="_blank"><?php print (render($content['field_reps_core_image']));?></a>
