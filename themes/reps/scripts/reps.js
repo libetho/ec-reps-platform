ffa024ac76f7ded3d6a9dc1de0eabb0e5d4a1eec
LIBERT Thomas <Thomas.LIBERT@ext.ec.europa.eu>
2015-10-09 11:20:13 +0200

    CR

--- themes/reps/scripts/reps.js
+++ themes/reps/scripts/reps.js
@@ -8,9 +8,6 @@
 jQuery(function($) {
   $(document).ready(function() {
     $('.caret').click(function() {
-      // console.log($(this).parents('.expanded').children('a'));
-      // $(this).parents('.expanded').children('a').attr('aria-expanded',true);
-      // var myClass = $(this).parents('.expanded').attr('class') + ' open';
       $(this).parents('.expanded').toggleClass('open');
     });
     $('.expanded a').click(function() {
