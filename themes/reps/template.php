8aa6eb1a14fd71755f98e4d2f2d174b7af5e96f4
LIBERT Thomas <Thomas.LIBERT@ext.ec.europa.eu>
2015-10-27 10:00:29 +0100

    comment corrected, sorry for the wrong username in my prvious commit

--- themes/reps/template.php
+++ themes/reps/template.php
@@ -133,7 +133,7 @@ function reps_menu_link($variables) {
   // Test if there is a sub menu.
   if ($element['#below'] && !theme_get_setting('disable_dropdown_menu') && !in_array('dropdown', $element['#attributes']['class'])) {
     // Menu item has sub menu.
-    // Add carret and class.
+    // Add class.
     $element['#attributes']['class'][] = 'dropdown';
 
     // Add attributes to children items.
