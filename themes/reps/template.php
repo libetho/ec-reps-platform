c77c558aa2ff8da56215881866868f79eba48573
LIBERT Thomas <Thomas.LIBERT@ext.ec.europa.eu>
2016-03-18 10:28:39 +0100

    fixes css for map and date format updated - 9731

--- themes/reps/template.php
+++ themes/reps/template.php
@@ -288,3 +288,34 @@ function reps_page_alter($page) {
   );
   drupal_add_html_head($meta_keywords, 'meta_keywords');
 }
+
+/**
+ * Returns HTML for a date element formatted as a range.
+ */
+function reps_date_display_range($variables) {
+  $date1 = $variables['date1'];
+  $date2 = $variables['date2'];
+  $timezone = $variables['timezone'];
+  $attributes_start = $variables['attributes_start'];
+  $attributes_end = $variables['attributes_end'];
+  $show_remaining_days = $variables['show_remaining_days'];
+
+  $start_date = '<span class="date-display-start"' . drupal_attributes($attributes_start) . '>' . $date1 . '</span>';
+  $end_date = '<span class="date-display-end"' . drupal_attributes($attributes_end) . '>' . $date2 . $timezone . '</span>';
+
+  // If microdata attributes for the start date property have been passed in,
+  // add the microdata in meta tags.
+  if (!empty($variables['add_microdata'])) {
+    $start_date .= '<meta' . drupal_attributes($variables['microdata']['value']['#attributes']) . '/>';
+    $end_date .= '<meta' . drupal_attributes($variables['microdata']['value2']['#attributes']) . '/>';
+  }
+
+  // Wrap the result with the attributes.
+  $output = '<div class="date-display-range">' . t('!start-date - !end-date', array(
+    '!start-date' => $start_date,
+    '!end-date' => $end_date,
+  )) . '</div>';
+
+  // Add remaining message and return.
+  return $output . $show_remaining_days;
+}
