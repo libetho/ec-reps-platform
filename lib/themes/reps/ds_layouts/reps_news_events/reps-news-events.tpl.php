7d07d31f99efdf4ce204f4c0df63e3967533f4df
serotonine <ks.serotonine@gmail.com>
2019-05-21 14:12:51 +0000

    REPR-1183 ds_layout> reps_new_event layout creation + assign to views

---
+++ lib/themes/reps/ds_layouts/reps_news_events/reps-news-events.tpl.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @file
+ * Template for Display Suite Reps News & Events layout.
+ */
+?>
+
+<div class="reps_news_events_wrapper">
+  
+    <?php if ($reps_ne_image): ?>
+        <div class="reps_ne_image">
+         <?php print $reps_ne_image; ?> 
+        </div>
+    <?php endif; ?>
+   <?php if ($reps_ne_title): ?>
+        <div class="reps_ne_title">
+            <?php print $reps_ne_title; ?>
+        </div>
+   <?php endif; ?>
+    <?php if ($reps_ne_body): ?>
+        <div class="reps_ne_body">
+            <?php print $reps_ne_body; ?>
+        </div>
+    <?php endif; ?>
+    
+    <?php if ($reps_ne_footer): ?>
+        <div class="reps_ne_footer">
+            <?php print $reps_ne_footer; ?> 
+        </div>
+    <?php endif; ?>
+
+</div>
+
+
